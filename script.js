e837f21041629e256dfe7e2c1f19dd3b2144962e
Meeran-O-Meter <meeran2resource@gmail.com>
2026-04-10 12:26:52 +0400

    Finished Calendar Functionality on desktop

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-const monthDisplay = document.getElementById("month-display");
-const timeDisplay = document.getElementById("time-display");
+document.addEventListener('DOMContentLoaded', function () {
+    let currentDate = new Date();
+    let today = new Date();
+    const monthDisplay = document.getElementById("month-display");
+    const timeDisplay = document.getElementById("time-display");
+    const calendarDates = document.getElementById("calendar-dates");
+    let currentMonth = currentDate.toLocaleDateString('default', {month: 'long'});
+    monthDisplay.innerHTML = currentMonth;
 
-let currentDate = new Date();
-let currentMonth = currentDate.toLocaleDateString('default', {month: 'long'});
-monthDisplay.innerHTML = currentMonth;
-
-
-function clock () {
-    const hour = currentDate.getHours()
-    const hours = hour < 10 ? "0" + hour : hours;
-    const minute = currentDate.getMinutes()
-    const minutes = minute < 10 ? "0" + minute : minute;
-    timeDisplay.innerHTML = hours + ":" + minutes
-    setTimeout(clock, 1000)
-}
-
+    function calendarRender(date) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstDay  = new Date(year, month, 1).getDay();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+        calendarDates.innerHTML = '';
 
 
-clock()
+        const previousMonthLastDay = new Date(year, month, 0).getDate()
+        for (let i = firstDay; i > 0; i--){
+            const dayDiv = document.createElement('div')
+            dayDiv.textContent = previousMonthLastDay - i + 1;
+            dayDiv.classList.add ('invisible')
+            calendarDates.appendChild(dayDiv)
+
+        }
+
+        for (let i = 1; i <= lastDay; i++) {
+            const dayDiv = document.createElement('div')
+            dayDiv.textContent = i
+            if (i === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
+                dayDiv.classList.add('today')
+            }
+            calendarDates.appendChild(dayDiv);
+        }
+
+    }
+
+    function clock () {
+        const hour = currentDate.getHours()
+        const hours = hour < 10 ? "0" + hour : hour;
+        const minute = currentDate.getMinutes()
+        const minutes = minute < 10 ? "0" + minute : minute;
+        timeDisplay.innerHTML = hours + ":" + minutes
+        setTimeout(clock, 1000)
+    };
+
+    clock()
+    calendarRender(currentDate)
+});
